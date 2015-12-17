b755528372c8bd88a2ef19185507a1fd987a7b10
jhmgbl <jhmgbl@gmail.com>
2015-12-17 17:30:08 +0100

    final

--- src/org/de/jmg/showips/ShowIPs.java
+++ src/org/de/jmg/showips/ShowIPs.java
@@ -76,15 +76,15 @@ import javax.swing.table.DefaultTableModel;
 				fc.setDialogTitle("Select log file");
 				  int returnVal = fc.showOpenDialog(button);
 				  if (returnVal == JFileChooser.APPROVE_OPTION) {
-					  String IPADDRESS_PATTERN = 
+					  final String IPADDRESS_PATTERN = 
 			                    "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
-			            String IP6PatternStd = "^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$";
-			            String IP6PatternCompr = "^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$";
-			            String IP6PatternAlt = "(?<![[:alnum:]]|[[:alnum:]]:)(?:(?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4}|(?:[a-f0-9]{1,4}:){1,6}:(?:[a-f0-9]{1,4}:){0,5}[a-f0-9]{1,4})(?![[:alnum:]]:?)";
-			            Pattern patternip4 = Pattern.compile(IPADDRESS_PATTERN);
-			            Pattern patternip6std = Pattern.compile(IP6PatternStd);
-			            Pattern patternip6compr = Pattern.compile(IP6PatternCompr);
-			            Pattern patternip6alt = Pattern.compile(IP6PatternAlt);
+			            final String IP6PatternStd = "^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$";
+			            final String IP6PatternCompr = "^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$";
+			            final String IP6PatternAlt = "(?<![[:alnum:]]|[[:alnum:]]:)(?:(?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4}|(?:[a-f0-9]{1,4}:){1,6}:(?:[a-f0-9]{1,4}:){0,5}[a-f0-9]{1,4})(?![[:alnum:]]:?)";
+			            final Pattern patternip4 = Pattern.compile(IPADDRESS_PATTERN);
+			            final Pattern patternip6std = Pattern.compile(IP6PatternStd);
+			            final Pattern patternip6compr = Pattern.compile(IP6PatternCompr);
+			            final Pattern patternip6alt = Pattern.compile(IP6PatternAlt);
 			            File file = fc.getSelectedFile();
 			            LinkedHashMap<String,foundIP> ips = new LinkedHashMap<>();
 			            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
